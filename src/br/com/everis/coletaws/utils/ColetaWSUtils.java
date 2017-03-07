3967a07009a530640be2ed726904aa616f1ec99f
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2017-03-07 18:32:31 -0300

    Otimizando a aplicação

---
+++ src/br/com/everis/coletaws/utils/ColetaWSUtils.java
@@ -0,0 +1,13 @@
+package br.com.everis.coletaws.utils;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class ColetaWSUtils {
+	
+}
