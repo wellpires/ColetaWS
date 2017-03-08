fc722f42c352abbd4d78bc0620fb16e25bb8782e
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2017-03-08 11:00:43 -0300

    Otimizando

--- src/br/com/everis/coletaws/amostrador/services/impl/AmostradorServiceImpl.java
+++ src/br/com/everis/coletaws/amostrador/services/impl/AmostradorServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 /**
  *
- * @author Wellington GonÃ§alves Pires
+ * @author Wellington Gonçalves Pires
  */
 public class AmostradorServiceImpl implements IAmostradorService {
     
